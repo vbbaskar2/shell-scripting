5ab3c85d62387fe330c0fb6907c94a04b26712ae
vbbaskaras <vbbaskar2@gmail.com>
2022-06-16 09:17:43 +0530

    modified files

--- 02-print.sh
+++ 02-print.sh
@@ -10,3 +10,26 @@ echo hello \n hello again
 # \n, \t
 echo -e "hello\nhello again"
 echo -e "hello\thello again\n hell0 3rd time"
+
+#colored output
+#syntax: echo -e "\[COLm Message"
+
+##COLOR  CODE
+#RED      31
+#GREEN    32
+#Yellow   33
+#Blue     34
+#Magenta  35
+#Cyan     36
+
+echo -e "\[31m This is Red"
+echo -e "\[32m This is Green"
+echo -e "\[33m This is Yellow"
+echo -e "\[34m This is Blue"
+echo -e "\[35m This is Magenta"
+echo -e "\[36m This is Cyan"
+
+
+
+
+
