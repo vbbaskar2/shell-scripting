c24c3fb8b635e000d6d1456db3439f6a86b1391c
vbbaskaras <vbbaskar2@gmail.com>
2022-06-17 08:55:29 +0530

    modified files

--- 05-sp-vars.sh
+++ 05-sp-vars.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# 0 is script name
 echo 0 = $0
+
+# First argument to script
 echo 1 = $1
 echo 2 = $2
 echo "* = $*"
 echo "@ = $@"
-echo "# =$#"
+echo "# = $#"
+
+echo -e "Your Name= $1\nYour Age =$2"
