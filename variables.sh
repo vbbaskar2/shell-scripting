4cdef07dda5f6be99e05fea7a1b447c7e320c13f
vbbaskaras <vbbaskar2@gmail.com>
2022-06-17 08:12:24 +0530

    modified files

---
+++ variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+##if we assign a name to a set of data, then that is called as variable
+
+#Number
+a=100
+
+#string
+b=abc
+
+#But in bash shell everything is considered as a string. Because there are no data types in Bash shell
+#we will Access the variables with $ prefixing the variable name.
+
+echo value of a is $a
+echo value of b is $b
+
