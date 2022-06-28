de41346bae78d285a3d3b48d62354c4d873d6174
vbbaskaras <vbbaskar2@gmail.com>
2022-06-28 08:18:37 +0530

    modified files

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,22 +6,24 @@ print(){
 
 stat(){
     if [ "$1" -eq 0 ]; then
-    echo -e "\e[1m successfully\e[0m"
+    echo -e "\e[1m success\e[0m" &>>$LOG
     else
-    echo -e  "\e[1m is not completed\e[0m"
+    echo -e  "\e[1m is not completed\e[0m" &>>$LOG
     fi
 }
+ LOG=/tmp/roboshop.log
+ rm -f $LOG
 
 print "Installing nginx"
-yum install nginx -y
+yum install nginx -y &>>$LOG
 stat $?
 
 print "Enabling nginx"
-systemctl enable nginx
+systemctl enable nginx &>>$LOG
 stat $?
 
 print "Starting nginx"
-systemctl start nginx
+systemctl start nginx &>>$LOG
 stat $?
 
 exit 1
