df1b21ac8d676dda1f6f57495021f26e0e5c6156
vbbaskaras <vbbaskar2@gmail.com>
2022-06-29 08:53:42 +0530

    updating shell-scripting

--- roboshop/components/create-instance.sh
+++ roboshop/components/create-instance.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-count =$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1"  | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
+COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1"  | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
 if [ $COUNT -eq 0 ]; then
   aws ec2 run-instances --image-id ami-0bb6af715826253bf --instance-type t3.micro --security-group-ids sg-09b5bda62389f79a2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
 else
