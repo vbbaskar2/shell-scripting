bd8c6a1281df64cbe247ff22abc5baa3ec249523
vbbaskaras <vbbaskar2@gmail.com>
2022-06-29 09:20:09 +0530

    updating shell-scripting

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -3,14 +3,14 @@
 source components/common.sh
 
 print "Download Repo"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
 stat $?
 
 print "Install MongoDB"
  yum install -y mongodb-org  &>>LOG
 stat $?
 
-sed  -i -e "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf &>>$LOG
+#sed  -i -e "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf &>>$LOG
 
 
 
