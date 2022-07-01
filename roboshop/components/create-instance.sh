201309da4e6b087fea9cf6d36a993373d59621fc
vbbaskaras <vbbaskar2@gmail.com>
2022-07-01 09:12:28 +0530

    updating shell-scripting

--- roboshop/components/create-instance.sh
+++ roboshop/components/create-instance.sh
@@ -8,7 +8,7 @@ fi
 
 sleep 5
 IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1"  | jq ".Reservations[].Instances[].PrivateIpAddress")
-sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}" record.json > /tmp/record.json
+sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json > /tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id Z0181693TYNQ0481BBOJ --change-batch file:///tmp/record.json | jq &>/dev/null
 
 
