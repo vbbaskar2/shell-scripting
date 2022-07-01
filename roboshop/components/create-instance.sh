70a87c7f8d5f5b3b3c0024d682243109bc8ac0e6
vbbaskaras <vbbaskar2@gmail.com>
2022-07-01 09:13:36 +0530

    updating shell-scripting

--- roboshop/components/create-instance.sh
+++ roboshop/components/create-instance.sh
@@ -3,7 +3,7 @@ COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1"  | jq ".R
 if [ $COUNT -eq 0 ]; then
   aws ec2 run-instances --image-id ami-0bb6af715826253bf --instance-type t3.micro --security-group-ids sg-09b5bda62389f79a2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
 else
-  echo -e "\e[1m $1Instance already exits\e[0m"
+  echo -e "\e[1m $1 Instance already exits\e[0m"
 fi
 
 sleep 5
