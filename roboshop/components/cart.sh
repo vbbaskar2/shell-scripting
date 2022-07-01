6cda9dea36d77edf829873bff20fcfe3dfe3700a
vbbaskaras <vbbaskar2@gmail.com>
2022-07-01 08:58:53 +0530

    updating shell-scripting

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -2,55 +2,5 @@
 
 source components/common.sh
 
-print "Installing NodeJs"
-yum install nodejs npm make gcc-c++ -y &>>$LOG
-stat $?
-
-
-print "Add Roboshop user"
-id roboshop &>>$LOG
-if [ $? -eq 0 ]; then
-  echo "User already exists"
-  else
-    useradd roboshop &>>$LOG
-fi
-stat $?
-
-print "Download Schema"
- curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>$LOG
-stat $?
-
-
-
-print "Extract schema"
-unzip -o -d /home/roboshop /tmp/cart.zip &>>$LOG
-stat $?
-
-print "Remove Older schema"
-rm -rf /home/roboshop/cart &>>$LOG
-stat $?
-
-print "Copy content"
- mv  /home/roboshop/cart-main /home/roboshop/cart
-stat $?
-
-print "install nodejs Dependencies"
-cd /home/roboshop/cart
-npm install --unsafe-perm &>>$LOG
-stat $?
-
-print "Fix APP permissions"
-chown -R roboshop:roboshop /home/roboshop
-stat $?
-
-print "Update MongoDB DNS Config"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/cart/systemd.service &>>$LOG
-stat $?
-
-print "Copy SystemD file"
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
-stat $?
-
-print "start Cart service"
-systemctl daemon-reload &>>$LOG && systemctl start cart &>>$LOG && systemctl enable cart &>>$LOG
-stat $?
+COMPONENT=cart
+NODEJS
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -13,5 +13,60 @@ stat(){
     fi
 }
 
+NODEJS(){
+  print "Installing NodeJs"
+  yum install nodejs npm make gcc-c++ -y &>>$LOG
+  stat $?
+
+
+  print "Add Roboshop user"
+  id roboshop &>>$LOG
+  if [ $? -eq 0 ]; then
+    echo "User already exists"
+    else
+      useradd roboshop &>>$LOG
+  fi
+  stat $?
+
+  print "Download Schema"
+   curl -s -L -o /tmp/${COMPONENET}.zip "https://github.com/roboshop-devops-project/${COMPONENET}/archive/main.zip" &>>$LOG
+  stat $?
+
+
+
+  print "Extract schema"
+  unzip -o -d /home/roboshop /tmp/${COMPONENET}.zip &>>$LOG
+  stat $?
+
+  print "Remove Older schema"
+  rm -rf /home/roboshop/${COMPONENET} &>>$LOG
+  stat $?
+
+  print "Copy content"
+   mv  /home/roboshop/${COMPONENET}-main /home/roboshop/${COMPONENET}
+  stat $?
+
+  print "install nodejs Dependencies"
+  cd /home/roboshop/${COMPONENET}
+  npm install --unsafe-perm &>>$LOG
+  stat $?
+
+  print "Fix APP permissions"
+  chown -R roboshop:roboshop /home/roboshop
+  stat $?
+
+  print "Update MongoDB DNS Config"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' /home/roboshop/${COMPONENET}/systemd.service &>>$LOG
+  stat $?
+
+  print "Copy SystemD file"
+  mv /home/roboshop/${COMPONENET}/systemd.service /etc/systemd/system/${COMPONENET}.service
+  stat $?
+
+  print "start Cart service"
+  systemctl daemon-reload &>>$LOG && systemctl start ${COMPONENET} &>>$LOG && systemctl enable ${COMPONENET} &>>$LOG
+  stat $?
+}
+
  LOG=/tmp/roboshop.log
  rm -f $LOG
